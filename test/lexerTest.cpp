4d142b8a68ed3575156bbd7faa88bf8a0297f432
CrazyGuy108 <taylor@hansenfamily.us>
2017-09-22 19:00:03 -0700

    Setup some testing stuff
    The testing framework being used is Google Test, which apparently
    requires fetching and building from source, preferably through a CMake
    ExternalProject, so that took a long time to get it working.
    
    On an unrelated note, randomly renamed the CMake option BUILD_DOCS to
    VSL_BUILD_DOCS for consistency with all the other variables.
    
    The LLVM library and the source files except src/main.cpp are put into
    a single CMake library target called 'libvsl' so that both the test
    program and the compiler itself can have libvsl linked to it without
    having to individually link all the object files.
    
    A decent amount of tests for Lexer and Parser were added as well.

---
+++ test/lexerTest.cpp
@@ -0,0 +1,34 @@
+#include "token.hpp"
+#include "vsllexer.hpp"
+#include "gtest/gtest.h"
+
+#define valid(src) EXPECT_TRUE(lex(src))
+#define invalid(src) EXPECT_FALSE(lex(src))
+
+namespace
+{
+// returns true if there's a lexical error, false otherwise
+bool lex(const char* src)
+{
+    VSLLexer lexer{ src };
+    while(!lexer.empty())
+    {
+        lexer.nextToken();
+    }
+    return !lexer.hasError();
+}
+} // namespace
+
+TEST(LexerTest, NotEmptyOnInit)
+{
+    VSLLexer lexer{ "hi" };
+    EXPECT_FALSE(lexer.empty());
+}
+
+TEST(LexerTest, HandlesNumbers)
+{
+    // small numberes are ok
+    valid("1337");
+    // but not big numbers like these
+    invalid("999999999999999999999999999999999");
+}
---
+++ test/parserTest.cpp
@@ -0,0 +1,61 @@
+#include "vsllexer.hpp"
+#include "vslparser.hpp"
+#include "gtest/gtest.h"
+
+#define valid(src) EXPECT_TRUE(parse(src))
+#define invalid(src) EXPECT_FALSE(parse(src))
+
+namespace
+{
+// returns true if there's a syntax error, false otherwise
+bool parse(const char* src)
+{
+    VSLLexer lexer{ src };
+    VSLParser parser{ lexer };
+    parser.parse();
+    return !parser.hasError();
+}
+} // namespace
+
+TEST(ParserTest, HandlesEmptyStatement)
+{
+    valid(";");
+    valid(";;;;;");
+}
+
+TEST(ParserTest, HandlesBlock)
+{
+    valid("{hi;}");
+    invalid("{hi;");
+}
+
+TEST(ParserTest, HandlesConditional)
+{
+    valid("if (x == 1) {;}");
+    invalid("if x == 1 {;}");
+}
+
+TEST(ParserTest, HandlesAssignment)
+{
+    valid("let x: Int = 1337;");
+    valid("var y: Void = x;");
+}
+
+TEST(ParserTest, HandlesFunction)
+{
+    valid("func f(x: Int) -> Void {;}");
+}
+
+TEST(ParserTest, HandlesReturn)
+{
+    valid("return x;");
+}
+
+TEST(ParserTest, HandlesExpr)
+{
+    valid("-x-1;");
+    invalid("x*;");
+    // needs a semicolon at the end
+    invalid("x");
+    valid("x(y: z % 2) - -z * 2 + 9 / 2;");
+}
